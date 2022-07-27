88253115a66a2988afc63ec8fb9d8abc183994c0
Crystal Korth <crystalkorth@gmail.com>
2022-07-27 07:56:43 -0500

    completed activity but bootstrap and pics not working

--- views/default.jsx
+++ views/default.jsx
@@ -1,18 +1,22 @@
 const React = require ('react')
 
-// function Def (html) {}
-
-module.exports = Def
 
 function Def (html) {
     return (
         <html>
+            
             <head>
                 <title>Title</title>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
+            
             <body>
                 {html.children}
             </body>
+            
         </html>
     )
 }
+
+module.exports = Def
--- views/error404.jsx
+++ views/error404.jsx
@@ -7,7 +7,14 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
+              <p>Ope, sorry, we can't find this page!</p>
+              <div>
+                <img src = "public/images/errorbike.jpg" alt = "Bike Lying Down on the Ground" />
+                <div>
+                Photo by <a href="https://unsplash.com/@ispywithmylittleeye?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Zeynep Sümer</a> on <a href="https://unsplash.com/s/photos/free-not-found?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+                  </div> 
+              </div>
           </main>
       </Def>
     )
