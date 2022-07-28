43334646e054e57c905e9a5f6810cad323ed116b
Crystal Korth <crystalkorth@gmail.com>
2022-07-27 21:19:36 -0500

    had my public file nested under my views file, corrected this and images are showing

--- views/home.jsx
+++ views/home.jsx
@@ -1,17 +1,19 @@
 const React = require ('react')
 const Def = require ('./default')
 
+
 function home () {
     return (
         <Def>
             <main>
                 <h1>HOME</h1>
                 <div>
-                    <img src="/images/taco.jpg" alt= "Tacos" />
-                    <div>
+                    <img className = "col-sm-12" src="/images/taco.jpg" alt= "Tacos" />
+                    {/* views\public\images\taco.jpg */}
+                    {/* <div>
                         Photo by <a href="https://unsplash.com/@edgarraw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Edgar Castrejon</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   
-                    </div>
+                    </div> */}
                 </div>
 
                 <a href="/places">
